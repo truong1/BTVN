68ec69031f1ec0250bf165e4535c2bdeb420a327
truong1 <hoanghatvs@gmail.com>
2017-06-26 23:28:59 +0700

    Update mainprojectsprite.js

--- mainprojectsprite.js
+++ mainprojectsprite.js
@@ -1,1 +1,7 @@
-
+goog.provide('TIC-TAC-TOE.Land');
+goog.require('lime.Sprite');
+TIC-TAC-TOE.Land = function(GmObj,PlayerXObj) {
+    goog.base(this);
+    this.setAnchorPoint(0, 0);
+    this.setSize(gameObj.tile_size,gameObj.tile_size);
+    this.setFill('images/Blank_title.jpeg');
