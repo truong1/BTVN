b12dd98f104312892c1d37a97160726f7cedf827
truong1 <hoanghatvs@gmail.com>
2017-06-26 22:05:29 +0700

    Update mainproject.js

--- mainproject.js
+++ mainproject.js
@@ -2,7 +2,9 @@ goog.provide('TIC-TAC-TOE')
 goog.require('lime.Referee')
 goog.require('lime.Player_X')
 goog.require('lime.Player_Y')
-goog.require('TIC.land')
+goog.require('lime.Layer')
+goog.require('TIC.Land')
 
 
 TIC-TAC_TOE.start = ()
+  var GmObject
