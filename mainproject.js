248d34a575b8cf28ea04b8f1f0e918bcf226f3bd
truong1 <hoanghatvs@gmail.com>
2017-06-26 23:21:38 +0700

    Update mainproject.js

--- mainproject.js
+++ mainproject.js
@@ -6,7 +6,20 @@ goog.require('lime.Layer')
 goog.require('TIC.Land')
 
 
-tictactoe.start = ()
-  var GmObject = {
+tictactoe.start = funct()
+  var GmObj = {
     width: 320,
     height: 500,
+    tile_size: 64,
+    num_tiles_x: 5,
+    num_tiles_y: 6,
+    landLayer_w: 64*5, 
+    landLayer_h: 64*6,
+    controlsLayer_w: 64*5,
+    controlsLayer_h: 64*1.5
+  }
+  var PlayerXObj = {
+    turn: 1,
+    currentSqrMark: 0
+  }
+  ga
